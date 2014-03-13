c310fac706eeb9b878c9a643c1e78164f986f800
LinusPhoenix <linus.eckhard@googlemail.com>
2014-03-13 23:26:04 +0100

    NEI start
    Getting started on NEI Support.

---
+++ src/main/java/FrogCraft/nei/NEIModule.java
@@ -0,0 +1,24 @@
+package FrogCraft.nei;
+
+import codechicken.nei.api.IConfigureNEI;
+
+public class NEIModule implements IConfigureNEI {
+
+	@Override
+	public void loadConfig() {
+		//TODO Machine RecipeHandlers
+		
+	}
+
+	@Override
+	public String getName() {
+		return "FrogCraft NEI Plugin";
+	}
+
+	@Override
+	public String getVersion() {
+
+		return "1.0";
+	}
+
+}
